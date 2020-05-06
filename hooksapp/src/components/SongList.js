7dad7878581fbd65044dfd7762c872702f4ec618
Sergiu Pop <sergiupop94@gmail.com>
2020-05-06 17:18:35 +0300

    Add initial hooks app

---
+++ hooksapp/src/components/SongList.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const SongList = () => {
+    return (
+        <div className="song-list">
+            <ul>
+                <li>Song 1</li>
+                <li>Song 2</li>
+            </ul>
+        </div>
+    );
+}
+ 
+export default SongList;
