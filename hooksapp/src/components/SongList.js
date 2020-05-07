fc268e0acb06ea7fc10ccd65e5e4f0ee5f62006f
Sergiu Pop <sergiupop94@gmail.com>
2020-05-07 15:28:23 +0300

    Integrate useState hook

--- hooksapp/src/components/SongList.js
+++ hooksapp/src/components/SongList.js
@@ -1,12 +1,28 @@
-import React from 'react';
+import React, { useState } from 'react';
+import uuid from 'uuid/v1';
 
 const SongList = () => {
+    const [songs, setSongs] = useState([
+        { title: 'Song1', id: 1 },
+        { title: 'Song2', id: 2 },
+        { title: 'Song3', id: 3 }
+    ]);
+    const addSong = () => {
+        setSongs([...songs, { title: 'Song4', id: uuid() }])
+    }
+
     return (
         <div className="song-list">
             <ul>
-                <li>Song 1</li>
-                <li>Song 2</li>
+                {
+                    songs.map(song => {
+                        return (
+                            <li key={song.id}>{song.title}</li>
+                        )
+                    })
+                }
             </ul>
+            <button onClick={addSong}>Add a song</button>
         </div>
     );
 }
