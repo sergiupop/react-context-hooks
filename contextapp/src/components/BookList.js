dd7951539ceb8c320a0e3d45cf6dcf8ed8d1c357
Sergiu Pop <sergiupop94@gmail.com>
2020-05-08 16:10:40 +0300

    Replace all class components with functional components

--- contextapp/src/components/BookList.js
+++ contextapp/src/components/BookList.js
@@ -16,22 +16,4 @@ const BookList = () => {
     )
 }
 
-/*class BookList extends Component {
-    static contextType = ThemeContext;
-    render() {
-        const { isLightTheme, light, dark } = this.context;
-        const theme = isLightTheme ? light : dark;
-
-        return (
-            <div className='book-list' style={{ background: theme.bg, color: theme.syntax }}>
-                <ul>
-                    <li style={{ background: theme.ui }}>Book1</li>
-                    <li style={{ background: theme.ui }}>Book2</li>
-                    <li style={{ background: theme.ui }}>Book3</li>
-                </ul>
-            </div>
-        )
-    }
-}*/
-
 export default BookList;
